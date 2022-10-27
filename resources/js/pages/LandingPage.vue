ab61f9318dffa42505c61ea8efade51476cd0ccd
FelXGit <entica.fel@gmail.com>
2022-10-27 16:35:27 +0800

    [UPDATE][USER] Chronoknowledge landing

---
+++ resources/js/pages/LandingPage.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="landingComponent" :class="[
+    { 'landingLoggedIn': isLoggedIn },
+    { 'landingGuest': !isLoggedIn }
+    ]">
+        <div v-show="isLoggedIn" class="sidebar-menu">
+            <Sidebar></Sidebar>
+        </div>
+        <div class="sidebar-views">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+import { getters, mutations, actions } from "../store";
+import Sidebar from '../components/main/Sidebar.vue'
+
+export default {
+    components: { Sidebar },
+    data() {
+        return {
+        };
+    },
+    methods: {
+        ...mutations, ...actions
+    },
+    computed: {
+        ...getters,
+        landingLoggedInStyle() {
+            return {
+                'grid-template-areas': `'sidebar posts posts categories''sidebarclasspapi posts posts popular'`,
+                'background-color': 'none',
+                'padding': '0'
+            }
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+@import "../../sass/imports";
+
+.landingLoggedIn {
+    display: grid;
+    grid-template-columns: auto auto;
+}
+.landingGuest {
+    display: grid;
+    grid-template-columns: auto;
+}
+
+</style>
---
+++ resources/js/pages/sidebar-views/PrivacyPolicy.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="main">
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  props: ['data']
+}
+</script>
+
+<style scoped>
+.main {
+  width: 100%;
+}
+</style>
